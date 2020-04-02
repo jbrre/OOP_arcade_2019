9e872e1bfcf97b74ccdf5019cc08d2bb6f451a7d
Noemie Kermel <noemie.kermel@epitech.eu>
2020-04-02 23:55:32 +0200

    [+] fuction display in libncurses

--- lib/srcs/libncurses.cpp
+++ lib/srcs/libncurses.cpp
@@ -21,6 +21,30 @@ libNCurses::~libNCurses()
 
 void libNCurses::display(std::vector<std::vector<square_status>> toDisplay) const
 {
+    int y = 10;
+
+    clear();
+    for (int i = 0; i < toDisplay.size(); i++, y++) {
+        for (int j = 0; j < toDisplay[i].size(); j++) {
+	    if (toDisplay[i][j] == EMPTY)
+	        mvprintw(y, (COLS/2), ' ');
+	    else if (toDisplay[i][j] == APPLE)
+	        mvprintw(y, (COLS/2), 'o');
+	    else if (toDisplay[i][j] == SNAKE_HEAD)
+		mvprintw(y, (COLS/2), 'p');
+	    else if (toDisplay[i][j] == SNAKE_BODY)
+		mvprintw(y, (COLS/2), 't');
+	    else if (toDisplay[i][j] == WALL)
+		mvprintw(y, (COLS/2), '|');
+	    else if (toDisplay[i][j] == PLAYER)
+		mvprintw(y, (COLS/2), 'P');
+	    else if (toDisplay[i][j] == GHOST)
+		mvprintw(y, (COLS/2), 'A');
+	    else if (toDisplay[i][j] == POINT)
+		mvprintw(y, (COLS/2), 'o');
+	}
+    }
+    refresh();
     return;
 }
 
