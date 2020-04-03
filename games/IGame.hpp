76159f0ed03b2de419378183fc11caf1e5cb4a54
Noemie Kermel <noemie.kermel@epitech.eu>
2020-04-03 23:34:11 +0200

    [MODIF] Put good name

---
+++ games/IGame.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2020
+** OOP_arcade_2019 [WSL: Ubuntu]
+** File description:
+** Igame
+*/
+
+#ifndef IGAME_HPP_
+#define IGAME_HPP_
+
+#include "../core/environment/Environment.hpp"
+
+class IGame {
+    protected:
+        int _score;
+        Environment *_env;
+        std::vector<std::vector<square_status>> _gameMap;
+    public:
+        virtual int game() = 0;
+        virtual std::map<std::string,Environment::sprite_t> *getSprites() = 0;
+        virtual void action() = 0;
+        virtual void setEnvironment(Environment *to_set) = 0;
+        virtual bool isGameOver() const = 0;
+        virtual int getScore() const = 0;
+        virtual std::string getGameName() const = 0;
+        virtual std::vector<std::vector<square_status>> getGameMap() const = 0;
+};
+
+typedef IGame *(*gameMaker)();
+
+#endif /* !IGAME_HPP_ */
