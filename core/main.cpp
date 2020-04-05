7b3a2132630b125d22e8405522ea567da165551e
Julien <julien.barrere@epitech.eu>
2020-04-05 18:01:39 +0200

    [MODIF] Put game over score

--- core/main.cpp
+++ core/main.cpp
@@ -20,10 +20,14 @@ int main(int ac, char **av)
         std::cerr << "Usage: ./arcade lib" << std::endl;
         return (84);
     }
+    std::cout << "Opening graphical lib..." << std::endl;
     arcade = new Core(av[1]);
+    std::cout << "Graphical lib open !" << std::endl;
     lib = arcade->getGraphs();
     arcade->loadGame(lib->menu());
+    std::cout << "Opening game lib..." << std::endl;
     game = arcade->getGame();
+    std::cout << "Game lib open !" << std::endl;
     arcade->setEnv(&general);
     game->setEnvironment(&general);
     lib->setEnvironment(&general);
@@ -33,6 +37,7 @@ int main(int ac, char **av)
         lib->display(game->getGameMap());
         usleep(LOOP_DELAY);
     }
+    std::cout << "Game over. Score :" << game->getScore() << std::endl;
     delete arcade;
     return (0);
 }
